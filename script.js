4bd132ba2ae4310521467b1af5d73b869dc50ecf
Anna Ossowska <anka.b.bogacka@gmail.com>
2024-03-25 11:01:58 +0100

    Add script to check palindrom

--- script.js
+++ script.js
@@ -4,9 +4,31 @@ const checkPalindromeBtn = document.getElementById('check-btn');
 const resultField = document.getElementById('result');
 
 // --- FUNCTIONS
-function cleanInputString(str) {
-    const regex = /[+-\s]/g;
-    return str.replace(regex, '');
+const palindromChecker = (input) => {
+    const originalInput =input;
+
+    resultField.replaceChildren();
+
+    if (userInput === '') {
+        alert('Please input a value');
+        return;
+    } 
+
+    const lowerCaseInput  = input.replace(/\s|[^a-zA-Z0-9]/g, '').toLowerCase();
+    console.log(lowerCaseInput);
+
+    let msg = `${originalInput} ${lowerCaseInput === [...lowerCaseInput].reverse().join('') ? 'is' : 'is not'} a palindrome.`;
+
+    const newPar = document.createElement('p');
+    newPar.className = 'user-input';
+    newPar.innerHTML = msg;
+    resultField.appendChild(newPar);
+
+    resultField.classList.remove('hidden');
 }
 
-// --- LISTENEERS
+// --- LISTENERS
+checkPalindromeBtn.addEventListener("click", () => {
+    palindromChecker(userInput.value);
+    userInput.value = '';
+});
